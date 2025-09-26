2335fdde40612b900b754ab8bd348a891c5ced2e
343GAUTAMVIJAY <147785180+343GAUTAMVIJAY@users.noreply.github.com>
2025-09-26 10:35:05 +0530

    Update Q1.js

--- assignment-2/Q1.js
+++ assignment-2/Q1.js
@@ -1,1 +1,25 @@
-k
+class BankAccount
+  def initialize(b)
+    @bal = b
+  end
+
+  def add_balance(a)
+    if a > 0
+      @bal += a
+    else
+      puts "Invalid deposit amount"
+    end
+  end
+
+  def withdraw(a)
+    if a > 0 && a <= @bal
+      @bal -= a
+    else
+      puts "Invalid withdraw amount"
+    end
+  end
+
+  def check_balance
+    @bal
+  end
+end
